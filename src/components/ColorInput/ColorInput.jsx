edd681c4448205d5e88ebf23d1a888e3be51a4b2
utk09-NCL <utkarsh.t0298@gmail.com>
2024-10-06 12:48:25 +0100

    feat: added toast message, bugfix HEX Input

--- src/components/ColorInput/ColorInput.jsx
+++ src/components/ColorInput/ColorInput.jsx
@@ -1,30 +1,28 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import chroma from "chroma-js";
+import { toast } from "react-hot-toast";
 import HEXColorInput from "./HEXColorInput";
 import HSLColorInput from "./HSLColorInput";
 import RGBColorInput from "./RGBColorInput";
 import ColorPreview from "./ColorPreview";
+import useDebounce from "../../hooks/useDebounce";
 
 const ColorInput = ({ colorValue, onChangeColor }) => {
   const [chromaColor, setChromaColor] = useState(chroma(colorValue));
 
   const [hexInput, setHexInput] = useState(colorValue);
+
+  const debouncedHexInput = useDebounce(hexInput, 500);
+
   const [rgbValues, setRgbValues] = useState(chromaColor.rgb());
   const [hslValues, setHslValues] = useState(chromaColor.hsl());
 
-  useEffect(() => {
-    if (chroma.valid(colorValue)) {
-      const newColor = chroma(colorValue);
-      setChromaColor(newColor);
-      setHexInput(colorValue);
-      setHslValues(newColor.hsl());
-      setRgbValues(newColor.rgb());
-    }
-  }, [colorValue]);
-
   const handleHexChange = (newHex) => {
-    setHexInput(newHex);
+    const validHex = /^#?[0-9A-Fa-f]*$/;
+    if (validHex.test(newHex)) {
+      setHexInput(newHex);
+    }
   };
 
   const handleRgbChange = (index, value) => {
@@ -52,6 +50,35 @@ const ColorInput = ({ colorValue, onChangeColor }) => {
 
     onChangeColor(newColor.hex());
   };
+
+  useEffect(() => {
+    if (chroma.valid(colorValue)) {
+      const newColor = chroma(colorValue);
+      setChromaColor(newColor);
+      setHexInput(colorValue);
+      setHslValues(newColor.hsl());
+      setRgbValues(newColor.rgb());
+    }
+  }, [colorValue]);
+
+  useEffect(() => {
+    if (debouncedHexInput === "") return;
+
+    if (chroma.valid(debouncedHexInput)) {
+      const newColor = chroma(debouncedHexInput);
+      setChromaColor(newColor);
+      setHslValues(newColor.hsl());
+      setRgbValues(newColor.rgb());
+
+      onChangeColor(debouncedHexInput);
+    } else {
+      toast.error(`Invalid HEX value - ${debouncedHexInput}`);
+    }
+
+    return () => {
+      toast.dismiss();
+    };
+  }, [debouncedHexInput, onChangeColor]);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-4 lg:gap-y-0">
