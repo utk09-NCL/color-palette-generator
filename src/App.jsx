032681f1073741ac62bf9c10b62ded8aab868d4c
utk09-NCL <utkarsh.t0298@gmail.com>
2024-10-06 15:12:36 +0100

    feat: added Generate Contrast Grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 import ColorInput from "./components/ColorInput/ColorInput";
 import "./App.css";
+import GenerateContrastGridColors from "./components/ContrastChecker/GenerateContrastGridColors";
 
 function App() {
   const [color, setColor] = useState("#4512ab");
+
+  const [generatedColor, setGeneratedColor] = useState(color);
+
+  const handleGenerateColors = () => {
+    setGeneratedColor(color);
+  };
 
   return (
     <div>
@@ -15,6 +22,18 @@ function App() {
         </h1>
         <section className="p-1 border-2 border-slate-300 rounded-xl mt-10">
           <ColorInput colorValue={color} onChangeColor={setColor} />
+          <div className="text-center mx-auto mt-6">
+            <button
+              onClick={handleGenerateColors}
+              className="px-4 py-2 border rounded-md border-slate-200 text-slate-500"
+              style={{ backgroundColor: color }}
+            >
+              Generate Colors
+            </button>
+            <div className="my-6">
+              <GenerateContrastGridColors baseColor={generatedColor} />
+            </div>
+          </div>
         </section>
       </main>
     </div>
