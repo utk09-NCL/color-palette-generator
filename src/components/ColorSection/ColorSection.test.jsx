b18918d748c15ff7562e7e718ff77b6958c0cdb1
Preet Shah <shahpreetk@gmail.com>
2024-10-13 15:00:06 +0100

    test: setup vitest, added tests for ColorSection component

---
+++ src/components/ColorSection/ColorSection.test.jsx
@@ -0,0 +1,86 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { render, fireEvent, screen } from "@testing-library/react";
+import { toast } from "react-hot-toast";
+
+import ColorSection from "./ColorSection";
+
+// Mock react-hot-toast
+vi.mock("react-hot-toast", () => ({
+  toast: {
+    error: vi.fn(),
+    dismiss: vi.fn(),
+  },
+}));
+
+describe("ColorSection", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  const onDeleteMock = vi.fn();
+  const onColorsGeneratedMock = vi.fn();
+
+  it("renders with default color name", () => {
+    const view = render(
+      <ColorSection
+        onDelete={onDeleteMock}
+        onColorsGenerated={onColorsGeneratedMock}
+      />,
+    );
+    expect(view.getByTestId("color-section-primary")).toBeInTheDocument();
+  });
+
+  it("renders with custom allowed color name", () => {
+    const view = render(
+      <ColorSection
+        initialColorName="secondary"
+        onDelete={onDeleteMock}
+        onColorsGenerated={onColorsGeneratedMock}
+      />,
+    );
+    expect(view.getByTestId("color-section-secondary")).toBeInTheDocument();
+  });
+
+  it("initially does not display generated colors", () => {
+    const view = render(
+      <ColorSection
+        onDelete={onDeleteMock}
+        onColorsGenerated={onColorsGeneratedMock}
+      />,
+    );
+    expect(
+      view.queryByTestId("generated-colors-primary"),
+    ).not.toBeInTheDocument();
+  });
+
+  it("displays generated colors when available", () => {
+    const view = render(
+      <ColorSection
+        onDelete={onDeleteMock}
+        onColorsGenerated={onColorsGeneratedMock}
+      />,
+    );
+
+    const generateColorsButton = view.getByTestId("generate-colors-btn");
+    fireEvent.click(generateColorsButton);
+
+    expect(view.getByTestId("generated-colors-primary")).toBeInTheDocument();
+  });
+
+  it("does not render with custom not allowed color name", async () => {
+    render(
+      <ColorSection
+        initialColorName="randomColorName"
+        onDelete={onDeleteMock}
+        onColorsGenerated={onColorsGeneratedMock}
+      />,
+    );
+
+    const generateColorsButton = screen.getByTestId("generate-colors-btn");
+    fireEvent.click(generateColorsButton);
+
+    expect(toast.error).toHaveBeenCalledWith(
+      "Invalid color name. Use one of the following: primary, secondary, tertiary, accent, info, success, warning, error, custom",
+    );
+  });
+});
