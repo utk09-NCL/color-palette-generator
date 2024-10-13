a99531fc4849a58bc874739a70fdf5d4b47d21fc
Preet Shah <shahpreetk@gmail.com>
2024-10-13 15:00:06 +0100

    test: setup vitest, added tests for ColorSection component

--- src/components/Shared/Button.jsx
+++ src/components/Shared/Button.jsx
@@ -10,11 +10,21 @@ import clsx from "clsx";
  * @param {string} props.className - Additional class names.
  * @param {function} props.onClick - Click handler function.
  * @param {boolean} props.disabled - Whether the button is disabled.
+ * @param {object} props.style - Additional inline styles.
+ * @param {string} props.dataTestid - The data-testid attribute value.
  * @returns {JSX.Element} The rendered component.
  */
-const Button = ({ children, className, onClick, disabled = false, style }) => {
+const Button = ({
+  children,
+  className,
+  onClick,
+  disabled = false,
+  style,
+  dataTestid,
+}) => {
   return (
     <button
+      data-testid={`${dataTestid}-btn`}
       className={clsx(
         "py-2 px-4 rounded-lg transition border-2 border-slate-200",
         disabled ? "opacity-50 cursor-not-allowed" : "",
@@ -35,6 +45,7 @@ Button.propTypes = {
   onClick: PropTypes.func,
   disabled: PropTypes.bool,
   style: PropTypes.object,
+  dataTestid: PropTypes.string,
 };
 
 export default Button;
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,5 +7,23 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    reporters: ["default", "junit"],
+    outputFile: { junit: "./reports/junit/junit.xml" },
+    setupFiles: ["vitest.setup.js"],
+    coverage: {
+      provider: "istanbul",
+      include: ["src/**/*"],
+      exclude: ["src/**/*.test.js*", "src/main.jsx", "src/App.jsx"],
+      reporter: ["text", "json-summary"],
+      thresholds: {
+        perFile: true,
+        statements: 100,
+        lines: 100,
+      },
+    },
   },
 });
