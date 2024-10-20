32eaef43bb59b56bbbf96ba7e677daecf4a7bea3
Shaurya Kushwaha <shaurya08k@gmail.com>
2024-10-20 23:03:17 +0530

    fix: remove duplicate imported react toast

--- src/components/ContrastChecker/GenerateContrastGridColors.jsx
+++ src/components/ContrastChecker/GenerateContrastGridColors.jsx
@@ -2,7 +2,6 @@
 
 import { useState, useMemo } from "react";
 import PropTypes from "prop-types";
-import { toast } from "react-hot-toast";
 import chroma, { scale } from "chroma-js";
 import { toast } from "react-hot-toast";
 
