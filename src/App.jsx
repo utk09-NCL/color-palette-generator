50b92e1b15343a2df25270d544958b7c19a2f881
samorobo <samgodwin111@gmail.com>
2024-10-25 13:03:25 +0100

    Feat:14(toggleButton): Added a toggle button to switch between light and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
-// src/App.jsx
 import { Toaster } from "react-hot-toast";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import { ModeToggle } from "./components/mode-toggle";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import "./styles/App.css";
-import Layout from "./components/Layout/Layout";
 
 // Create a browser router
 const router = createBrowserRouter([
   {
+    id: "home",
     path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-        errorElement: <NotFound />,
-      },
-      {
-        id: "not-found",
-        path: "*",
-        element: <NotFound />,
-      },
-      // Add more routes here as needed
-    ],
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+  {
+    id: "not-found",
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
@@ -34,11 +26,16 @@ const router = createBrowserRouter([
  * @returns {JSX.Element} The rendered App component.
  */
 function App() {
+  //passed the ModeToggle component here so it can be seen in the application
   return (
     <div className="App">
       {/* Render toast notifications */}
-      <Toaster />
-      <RouterProvider router={router} />
+      <div className="flex justify-end">
+        <Toaster />
+        <RouterProvider router={router} />
+        <ModeToggle />
+      </div>
+
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-// src/main.jsx
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App.jsx";
+import {ThemeProvider} from "./components/theme-provider";
 
+//Wrapping the App component with the ThemeProvider component so that its can reflect everywhere on the code   with the App component acting as the children
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme"> 
+      <App />
+     </ThemeProvider> 
   </StrictMode>,
 );
